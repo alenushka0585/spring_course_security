e8e1f9b127e378277fe280f7006ea1709e14922a
Yelena Kaloshina <yelena_kaloshina@epam.com>
2021-01-07 17:03:14 +0600

    Initial commit

---
+++ src/main/java/com/epam/spring/security/configuration/MySecurityConfig.java
@@ -0,0 +1,47 @@
+package com.epam.spring.security.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.User.UserBuilder;
+
+import javax.sql.DataSource;
+
+@EnableWebSecurity
+public class MySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource);
+
+        //        UserBuilder userBuilder = User.withDefaultPasswordEncoder();
+//
+//        auth.inMemoryAuthentication()
+//                .withUser(userBuilder.
+//                        username("yelena").
+//                        password("yelena").roles("EMPLOYEE"))
+//                .withUser(userBuilder.
+//                        username("zaur").
+//                        password("zaur").roles("MANAGER", "HR"))
+//                .withUser(userBuilder.
+//                        username("yarina").
+//                        password("yarina").roles("HR"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/").hasAnyRole("EMPLOYEE", "HR", "MANAGER")
+                .antMatchers("/hr_info").hasRole("HR")
+                .antMatchers("/manager_info/**").hasRole("MANAGER")
+                .and().formLogin().permitAll();
+    }
+}
